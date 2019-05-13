55f068d3b03c6c0e5152266d2a1ffe32a92c13b9
Mike Driscoll <mike@pythonlibrary.org>
2019-05-13 13:04:39 -0500

    switch back to using textctrl instead of richtext

--- main.py
+++ main.py
@@ -10,35 +10,10 @@ class FilePanel(wx.Panel):
     def create_ui(self):
         main_sizer = wx.BoxSizer(wx.VERTICAL)
 
-        self.text_ctrl = rt.RichTextCtrl(self, style=wx.VSCROLL|wx.HSCROLL|wx.NO_BORDER)
+        self.text_ctrl = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         main_sizer.Add(self.text_ctrl, 1, wx.ALL | wx.EXPAND, 5)
 
         self.SetSizer(main_sizer)
-
-    def OnFont(self, evt):
-        """
-        MODIFY THIS CODE FROM DEMO
-        """
-        if not self.rtc.HasSelection():
-            return
-
-        r = self.rtc.GetSelectionRange()
-        fontData = wx.FontData()
-        fontData.EnableEffects(False)
-        attr = wx.TextAttr()
-        attr.SetFlags(wx.TEXT_ATTR_FONT)
-        if self.rtc.GetStyle(self.rtc.GetInsertionPoint(), attr):
-            fontData.SetInitialFont(attr.GetFont())
-
-        dlg = wx.FontDialog(self, fontData)
-        if dlg.ShowModal() == wx.ID_OK:
-            fontData = dlg.GetFontData()
-            font = fontData.GetChosenFont()
-            if font:
-                attr.SetFlags(wx.TEXT_ATTR_FONT)
-                attr.SetFont(font)
-                self.rtc.SetStyle(r, attr)
-        dlg.Destroy()
 
 
 class MainPanel(wx.Panel):
@@ -56,9 +31,6 @@ class MainPanel(wx.Panel):
             notebook.AddPage(tab_panel, f'File {tab+1}')
         main_sizer.Add(notebook, 1, wx.ALL | wx.EXPAND, 5)
         self.SetSizer(main_sizer)
-
-
-
 
 
 class MainFrame(wx.Frame):
